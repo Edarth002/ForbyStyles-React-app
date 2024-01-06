b6e9debd6798544da2e559ec543ae938271d6ae4
Arthur <arthurwriting717@gmail.com>
2024-01-06 11:55:43 -0800

    project commit after first attempt failed

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const targetDate = new Date('February 23, 2024 08:00:00');
+  const targetDate = new Date( 'February 23, 2024 08:00:00');
   return (
     <Router>
       <Routes>
         {/* Index route */}
         <Route path="/" element={<Hero targetDate={targetDate}/>} />
-
-        {/* Other routes */}
-        <Route path="/blog" element={<BlogPage/>} />
-        <Route path="/blog/:blog_title" element={<BlogPage/>} />
       </Routes>
     </Router>
   );
